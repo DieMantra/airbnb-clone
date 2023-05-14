1904d0b9cc31f0c6b10b1a791e912f7884936542
Tyson <100465135+DieMantra@users.noreply.github.com>
2023-05-14 18:38:55 +1000

    added apple style modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
-import { motion, useDragControls } from 'framer-motion';
-import { PointerEvent, useRef } from 'react';
+import { motion } from 'framer-motion';
+import { PointerEvent, useEffect, useRef } from 'react';
 import isMobileDevice from '../../utils/DetectMobile';
 import styles from './Modal.module.scss';
 
@@ -8,23 +8,62 @@ interface ModalProps {
   isOpen: boolean;
   setIsOpen: (e: boolean) => void;
 }
+let startHeight = 0;
 
 function Modal({ children, setIsOpen }: ModalProps) {
   const html = document.documentElement;
   const root = document.getElementById('root') as HTMLDivElement;
   const dragRef = useRef<HTMLDivElement>(null);
   const isMobile = isMobileDevice();
-  const dragControls = useDragControls();
+  const MODAL_HEIGHT = 92;
 
-  function startDrag(event: PointerEvent<HTMLDivElement>) {
-    dragControls.start(event, { snapToCursor: false });
+  let startY = 0;
+  let distranceY = 0;
+  let closeFromDraggin = false;
+
+  useEffect(() => {
+    startHeight = dragRef.current?.clientHeight || 0;
+  }, []);
+
+  function handleDragStart(event: PointerEvent<HTMLDivElement>) {
+    startY = event.clientY;
   }
-  function stopDrag(event: PointerEvent<HTMLDivElement>) {
-    if (
-      dragRef.current?.clientHeight &&
-      event.clientY > dragRef.current?.clientHeight - 100
-    ) {
+
+  function handleDragging(event: PointerEvent<HTMLDivElement>) {
+    distranceY = Math.round(event.clientY - startY);
+    if (distranceY > 0 && dragRef.current) {
+      const newHeightInPixels = startHeight - distranceY;
+      const oldHeightInPixels = startHeight;
+
+      const percentageChanged =
+        ((newHeightInPixels - oldHeightInPixels) / oldHeightInPixels) * 100;
+      const newHeightInPercentage = MODAL_HEIGHT + percentageChanged;
+      dragRef.current.style.height = `${newHeightInPercentage}%`;
+
+      const newRootTranslateY = Math.round((percentageChanged + 100) / 10);
+
+      const newRootScaleAmout = Math.min(
+        Math.max(1 - (percentageChanged + 100) / 1000 + 0.05, 0),
+        1,
+      );
+      setStyle(root, {
+        transform: `translateY(${newRootTranslateY}px) scale(${newRootScaleAmout})`,
+      });
+      if (percentageChanged + 100 < 50) {
+        closeFromDraggin = true;
+      } else {
+        closeFromDraggin = false;
+      }
+    }
+  }
+
+  function handleDragEnd() {
+    if (closeFromDraggin) {
       setIsOpen(false);
+    } else {
+      dragRef.current?.style.removeProperty('height');
+      setHtmlStyle();
+      setRootStyles();
     }
   }
 
@@ -59,33 +98,21 @@ function Modal({ children, setIsOpen }: ModalProps) {
       >
         <motion.div
           ref={dragRef}
-          drag="y"
-          onPointerDown={startDrag}
-          onPointerUp={stopDrag}
-          dragControls={dragControls}
-          dragSnapToOrigin
           className={styles.container}
           variants={modalVariants}
           initial="closed"
           animate="open"
           exit="closed"
-          transition={{ duration: 0.35, ease: 'easeInOut' }}
+          style={{
+            transition: 'height 0.2s linear',
+            height: `${MODAL_HEIGHT}%`,
+          }}
+          transition={{ duration: 0.25, ease: 'linear' }}
           onAnimationStart={(variant) => {
             if (variant === 'open' && isMobile) {
-              setStyle(html, {
-                transition: 'none',
-                backgroundColor: 'var(--bg-additive-5x)',
-              });
-              setStyle(root, {
-                transition: 'transform 0.3s ease-in-out',
-                transform: 'translateY(1%) scale(0.95)',
-                minHeight: '100vh',
-                borderTopLeftRadius: '1.5rem',
-                borderTopRightRadius: '1.5rem',
-                boxShadow: '0 -15px 50px -12px rgb(0 0 0 / 0.25)',
-              });
-            }
-            if (variant !== 'open' && isMobile) {
+              setHtmlStyle();
+              setRootStyles();
+            } else {
               reset(html);
               reset(root, ['transform']);
               reset(root, ['borderTopLeftRadius', 'borderTopRightRadius']);
@@ -95,9 +122,16 @@ function Modal({ children, setIsOpen }: ModalProps) {
             if (variant === 'closed' && isMobile) {
               reset(html);
               reset(root);
+              root.attributes.removeNamedItem('style');
             }
           }}
         >
+          <motion.span
+            onPointerDown={handleDragStart}
+            onPointerUp={handleDragEnd}
+            onPointerMove={handleDragging}
+            className={styles.dragBar}
+          />
           {children}
         </motion.div>
       </motion.div>
@@ -105,6 +139,29 @@ function Modal({ children, setIsOpen }: ModalProps) {
   );
 }
 export default Modal;
+
+function setHtmlStyle(styles?: Styles) {
+  const html = document.documentElement;
+
+  setStyle(html, {
+    transition: 'none',
+    backgroundColor: 'var(--bg-additive-5x)',
+    ...styles,
+  });
+}
+function setRootStyles(styles?: Styles) {
+  const root = document.getElementById('root') as HTMLDivElement;
+
+  setStyle(root, {
+    transition: 'transform 0.1s linear',
+    transform: 'translateY(10px) scale(0.95)',
+    minHeight: '100vh',
+    borderTopLeftRadius: '1.5rem',
+    borderTopRightRadius: '1.5rem',
+    boxShadow: '0 -15px 50px -12px rgb(0 0 0 / 0.25)',
+    ...styles,
+  });
+}
 
 const cache = new Map();
 
