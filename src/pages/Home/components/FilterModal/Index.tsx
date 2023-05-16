6c99e00412482497493aec36f98bb7f501b09653
Tyson <100465135+DieMantra@users.noreply.github.com>
2023-05-16 20:38:36 +1000

    nicer looking modal form

--- src/pages/Home/components/FilterModal/Index.tsx
+++ src/pages/Home/components/FilterModal/Index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import Modal from '../../../../components/Modal/Modal';
 import styles from './FilterModal.module.scss';
 interface IndexProps {
@@ -7,15 +7,50 @@ interface IndexProps {
 }
 
 function Index({ modalIsOpen, setModalIsOpen }: IndexProps) {
+  const [inputs, setInputs] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+  });
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const isDisabled = Object.values(inputs).some((value) => value.length < 3);
+
   return (
     <Modal isOpen={modalIsOpen} setIsOpen={setModalIsOpen}>
       <div className={styles.container}>
         <h1>Sign up</h1>
         <p>Sign up to get access to the latest news and updates</p>
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="Email" />
-        <button className={styles.submitbtn}>Sign up</button>
+        <input
+          onChange={handleInput}
+          name="firstname"
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleInput}
+          name="lastname"
+          type="text"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleInput}
+          name="email"
+          type="text"
+          placeholder="Email"
+        />
+
+        <button disabled={isDisabled} className={styles.submitbtn}>
+          Sign up
+        </button>
         <button
           className={styles.cancelBtn}
           onClick={setModalIsOpen.bind(null, false)}
