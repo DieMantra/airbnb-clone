7791e55ec130e5b1ef176aeb4ca9ed507a5c99c7
Tyson <100465135+DieMantra@users.noreply.github.com>
2023-05-21 17:31:09 +1000

    working with react aria focus

--- src/pages/Home/components/Header/UserControls.tsx
+++ src/pages/Home/components/Header/UserControls.tsx
@@ -1,5 +1,12 @@
-import { useRef, useState } from 'react';
-import { useButton } from 'react-aria';
+import { KeyboardEvent, ReactNode, useEffect, useRef, useState } from 'react';
+import {
+  FocusScope,
+  useButton,
+  useFocus,
+  useFocusManager,
+  useFocusWithin,
+  useHover,
+} from 'react-aria';
 import { FaUserCircle } from 'react-icons/fa';
 import { FiGlobe } from 'react-icons/fi';
 import Modal from '../../../../components/Modal/Modal';
@@ -51,46 +58,111 @@ function LanguageSelectionModal() {
 
 function UserLogin() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const { focusProps } = useFocus({
+    onFocusChange: (focuse) => setIsFocused(focuse),
+  });
+  const { isHovered, hoverProps } = useHover({});
   const { buttonProps } = useButton(
     {
-      onPress: handleOnClick,
+      onPress: () => setIsOpen((prev) => !prev),
     },
     buttonRef,
   );
 
-  function handleOnClick() {
-    setIsOpen((prev) => !prev);
-  }
-
   return (
-    <button
-      className={styles.userLoginContainer}
-      ref={buttonRef}
-      {...buttonProps}
-    >
-      <div className={styles.hamburger}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <FaUserCircle />
-      {isOpen ? <Controls /> : ''}
-    </button>
+    <div className={styles.userLoginContainer}>
+      <button
+        className={`${styles.userLoginButton} ${
+          isOpen || isHovered ? styles.open : ''
+        } ${isFocused ? styles.active : ''}`}
+        ref={buttonRef}
+        {...buttonProps}
+        {...hoverProps}
+        {...focusProps}
+        tabIndex={0}
+      >
+        <div className={styles.hamburger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <FaUserCircle />
+      </button>
+      {isOpen ? <Controls setIsOpen={setIsOpen} /> : ''}
+    </div>
   );
 }
-function Controls() {
+function Controls({ setIsOpen }: { setIsOpen: (e: boolean) => void }) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOnClick = (e: MouseEvent) => {
+      if (modalRef.current?.contains(e.target as Node)) return;
+      setIsOpen(false);
+    };
+    document.addEventListener('click', handleOnClick);
+    return () => document.removeEventListener('click', handleOnClick);
+  }, [setIsOpen]);
+
+  const Link = ({
+    children,
+    isBold,
+  }: {
+    children: ReactNode;
+    isBold?: boolean;
+  }) => {
+    const [isFocused, setIsFocused] = useState(false);
+    const { isHovered, hoverProps } = useHover({});
+    const focusManager = useFocusManager();
+    const { focusWithinProps } = useFocusWithin({
+      onFocusWithinChange: (isFocusWithin) => setIsFocused(isFocusWithin),
+    });
+
+    const onKeyDown = (e: KeyboardEvent<HTMLAnchorElement>) => {
+      if (e.key === 'ArrowRight') {
+        focusManager.focusNext({ wrap: true });
+        return;
+      }
+      if (e.key === 'ArrowLeft') {
+        focusManager.focusPrevious({ wrap: true });
+        return;
+      }
+      if (e.key === 'Escape') {
+        setIsOpen(false);
+        return;
+      }
+    };
+
+    return (
+      <a
+        {...focusWithinProps}
+        {...hoverProps}
+        className={`${isBold ? styles.bold : ''} ${styles.link} ${
+          isHovered ? styles.hovered : ''
+        } ${isFocused ? styles.focused : ''}`}
+        tabIndex={0}
+        onKeyDown={onKeyDown}
+      >
+        {children}
+      </a>
+    );
+  };
+
   return (
-    <div className={styles.userLoginModalContaienr}>
-      <div className={styles.buttonWrapper}>
-        <button className={styles.signupInModalButton}>Sign up</button>
-        <button>Log in</button>
+    <FocusScope contain>
+      <div ref={modalRef} className={styles.userLoginModalContaienr}>
+        <div className={styles.buttonWrapper}>
+          <Link isBold>Sign up</Link>
+          <Link>Log in</Link>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.buttonWrapper}>
+          <Link>Airbnb your home</Link>
+          <Link>Help</Link>
+        </div>
       </div>
-      <div className={styles.divider}></div>
-      <div className={styles.buttonWrapper}>
-        <button>Airbnb your home</button>
-        <button>Help</button>
-      </div>
-    </div>
+    </FocusScope>
   );
 }
